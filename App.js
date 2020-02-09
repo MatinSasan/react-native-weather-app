cc5bdc1493c8e58128650d1cf1461f5b7a9c09e2
Matin Sasanpour <matin.sasan@gmail.com>
2020-02-09 18:07:38 +0330

    foundation of the app put

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+import {api} from './utils/api';
+import dateBuilder from './utils/databuilder';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
