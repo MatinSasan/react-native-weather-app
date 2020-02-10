ca2786695ba4b913f3b0bcdf8ef5d9e1e2bdf536
Matin Sasanpour <matin.sasan@gmail.com>
2020-02-10 14:39:05 +0330

    implemented data recieved from api into app page | search, onChange functions working | must work on CSS style next

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-import React, {useState} from 'react';
-import {StyleSheet, View, Text, ImageBackground, TextInput} from 'react-native';
+import React, {useState, Fragment} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ImageBackground,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 
 import {api} from './utils/api';
 import dateBuilder from './utils/databuilder';
@@ -9,26 +16,26 @@ const App = () => {
   const [weather, setWeather] = useState({});
   const [alert, setAlert] = useState(null);
 
-  const search = async e => {
+  const search = async () => {
     try {
-      if (e.key === 'enter') {
-        const res = await fetch(
-          `${api.base}weather?q=${query}&units=metric&APPID=${api.key}`,
-        );
-        const result = await res.json();
-        if (result.cod === '404') {
-          setAlert(result.message);
-          setTimeout(() => {
-            setAlert(null);
-          }, 3000);
-        }
-        setWeather(result);
-        setQuery('');
+      const res = await fetch(
+        `${api.base}weather?q=${query}&units=metric&APPID=${api.key}`,
+      );
+      const result = await res.json();
+      if (result.cod === '404') {
+        setAlert(result.message);
+        setTimeout(() => {
+          setAlert(null);
+        }, 3000);
       }
+      setWeather(result);
+      setQuery('');
     } catch (err) {
       console.error(err);
     }
   };
+
+  const onChange = e => setQuery(e);
 
   const imageShift =
     typeof weather.main !== 'undefined'
@@ -43,10 +50,30 @@ const App = () => {
         source={imageShift}
         style={{width: '100%', height: '100%'}}>
         <View style={styles.searchBox}>
-          <TextInput placeholder="Search..." style={styles.searchBar} />
+          <TextInput
+            onChangeText={onChange}
+            placeholder="Search..."
+            style={styles.searchBar}
+          />
+          <TouchableOpacity onPress={search}>
+            <Text>Search</Text>
+          </TouchableOpacity>
         </View>
         <View>
           <Text>{dateBuilder(new Date())}</Text>
+          {typeof weather.main !== 'undefined' ? (
+            <Fragment>
+              <Text>
+                {weather.name}, {weather.sys.country}
+              </Text>
+              <Text>{Math.round(weather.main.temp)}°c</Text>
+              <Text>{weather.weather[0].main}</Text>
+            </Fragment>
+          ) : alert !== null ? (
+            <Text>{alert}</Text>
+          ) : (
+            <Text>{''}</Text>
+          )}
         </View>
       </ImageBackground>
     </View>
